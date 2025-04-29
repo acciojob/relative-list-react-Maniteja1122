ff7d6f8099ba1be7595b8403e3cc3f5c5141fda8
ManitejaVipani <maniteja@vipani.co>
2025-04-29 13:40:01 +0530

    push

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,12 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+      <ol key="relativeList">
+        <li key="relativeListItem1">Emmanuel</li>
+        <li key="relativeListItem2">umesh</li>
+        <li key="relativeListItem3">rajshekar</li>
+        <li key="relativeListItem4">sai</li>
+      </ol>
     </div>
   )
 }
